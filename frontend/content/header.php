b7ebb9424faf22df9b3e9bb00c6e4c47419ab09a
Renan Fernando <renanfernando055@gmail.com>
2021-09-19 23:48:46 -0300

    Algumas modificações nos ícones do Header e Carousel

--- frontend/content/header.php
+++ frontend/content/header.php
@@ -1,12 +1,15 @@
 <div id="header" class="border-bottom border-2 py-3">
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light">
+
             <a class="navbar-brand" href="#">
                 <img src="../frontend/assets/img/svg/logo.svg" class="img-fluid" width="60" height="auto" alt="">
             </a>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse justify-content-between text-center" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item pe-3">
@@ -23,37 +26,19 @@
                     </li>
                 </ul>
 
-                <div class="row align-items-center">
+                <div class="row align-items-center mt-lg-1 mt-sm-4">
 
-                    <!-- Facebook -->
-                    <div class="col-sm-3 border-end border-3">
+                    <!-- Perfil -->
+                    <div class="col-sm-6 px-4 border-end border-3">
                         <span class="p-1">
                             <a href="#">
-                                <i class="fab fa-facebook fa-2x" aria-hidden="true"></i>
-                            </a>
-                        </span>
-                    </div>
-
-                    <!-- Twitter -->
-                    <div class="col-sm-3 border-end border-3">
-                        <span class="p-1">
-                            <a href="#">
-                                <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
-                            </a>
-                        </span>
-                    </div>
-
-                    <!-- Instagram -->
-                    <div class="col-sm-3 border-end border-3">
-                        <span class="p-1">
-                            <a href="#">
-                                <i class="fab fa-instagram fa-2x" aria-hidden="true"></i>
+                                <i class="fas fa-user fa-2x" aria-hidden="true"></i>
                             </a>
                         </span>
                     </div>
 
                     <!-- Busca -->
-                    <div class="col-sm-3">
+                    <div class="col-sm-6 px-4">
                         <span class="p-1">
                             <a href="#">
                                 <i class="fas fa-search fa-2x" aria-hidden="true"></i>
@@ -61,6 +46,7 @@
                         </span>
                     </div>
                 </div>
+
             </div>
         </nav>
     </div>
