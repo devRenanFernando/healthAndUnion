e1d35fd8df57313338fdbdbb2382291b9c92b21f
Renan Fernando <renanfernando055@gmail.com>
2021-09-23 11:08:30 -0300

    Formatação da page Profile

--- frontend/content/profile.php
+++ frontend/content/profile.php
@@ -1,61 +1,102 @@
-<section id="profile">
-    <div class="container">
-        <div id="first-content" class="row">
-            <div class="col-md-6">
-            <h2 class="title title-primary">welcome back!</h2>
-                <p class="description description-primary">To keep connected with us</p>
-                <p class="description description-primary">please login with your personal info</p>
-                <button id="signin" class="btn btn-primary">sign in</button>
-            </div>
-            <div class="col-md-6">
-            <h2 class="title title-second">create account</h2>
-                <div class="social-media">
-                    <ul class="list-social-media">
-                        <a class="link-social-media" href="#">
-                            <li class="item-social-media">
-                                <i class="fab fa-facebook-f"></i>        
-                            </li>
-                        </a>
-                        <a class="link-social-media" href="#">
-                            <li class="item-social-media">
-                                <i class="fab fa-google-plus-g"></i>
-                            </li>
-                        </a>
-                        <a class="link-social-media" href="#">
-                            <li class="item-social-media">
-                                <i class="fab fa-linkedin-in"></i>
-                            </li>
-                        </a>
-                    </ul>
-                </div><!-- social media -->
-                <p class="description description-second">or use your email for registration:</p>
-                <form class="form">
-                    <label class="label-input" for="">
-                        <i class="far fa-user icon-modify"></i>
-                        <input type="text" placeholder="Name">
-                    </label>
-                    
-                    <label class="label-input" for="">
-                        <i class="far fa-envelope icon-modify"></i>
-                        <input type="email" placeholder="Email">
-                    </label>
-                    
-                    <label class="label-input" for="">
-                        <i class="fas fa-lock icon-modify"></i>
-                        <input type="password" placeholder="Password">
-                    </label>
-                    
-                    
-                    <button class="btn btn-second">sign up</button>        
-                </form>
-            </div>
-        </div>
+<section id="profile" class="container-flex min-vh-100 d-flex align-items-center justify-content-center ">
+        <div class="w-75 rounded-3">
+            <div id="first-content" class="row content py-5 w-100 d-flex align-items-center">
+                <div class="first-column col-md-4 col-12 text-center text-light">
+                    <h2 class="title title-primary fs-2 text-capitalize">Bem vindo de volta!</h2>
+                    <p class="description description-primary">Para manter conectado conosco</p>
+                    <p class="description description-primary">Por favor, faça o login com suas informações pessoais </p>
+                    <button id="signin" class="btn btn-primary">Sign in</button>
+                </div>
+                <div class="second-column col-md-8 col-12 text-center text-light">
+                    <h2 class="title title-second">Criar Conta</h2>
+                    <div class="social-media">
+                        <ul class="list-social-media ">
+                            <a class="link-social-media" href="#">
+                                <li class="item-social-media">
+                                    <i class="fab fa-facebook-f"></i>
+                                </li>
+                            </a>
+                            <a class="link-social-media" href="#">
+                                <li class="item-social-media">
+                                    <i class="fab fa-google"></i>
+                                </li>
+                            </a>
+                            <a class="link-social-media" href="#">
+                                <li class="item-social-media">
+                                    <i class="fab fa-linkedin-in"></i>
+                                </li>
+                            </a>
+                        </ul>
+                    </div><!-- social media -->
+                    <p class="description description-second">Ou use seu e-mail para entrar:</p>
+                    <form class="form d-flex flex-column">
+                        <label class="label-input" for="">
+                            <i class="far fa-user icon-modify"></i>
+                            <input type="text" placeholder="Nome">
+                        </label>
 
-        <div id="second-content" class="row">
-            <col-md-6>
-            </col-md-6>
-            <col-md-6>
-            </col-md-6>
+                        <label class="label-input" for="">
+                            <i class="far fa-envelope icon-modify"></i>
+                            <input type="email" placeholder="Email">
+                        </label>
+
+                        <label class="label-input" for="">
+                            <i class="fas fa-lock icon-modify"></i>
+                            <input type="password" placeholder="Senha">
+                        </label>
+
+
+                        <button class="btn btn-second">Sign up</button>
+                    </form>
+                </div>
+            </div>
+
+            <div id="second-content content" class="row position-absolute d-none">
+                <div class="col-md-6">
+                    <h2 class="title title-primary">Olá, Amigo!</h2>
+                    <p class="description description-primary">Digite seus dados pessoais</p>
+                    <button id="signin" class="btn btn-primary">Sign up</button>
+                </div>
+                <div class="col-md-6">
+                    <h2 class="title title-second">Faça login para ficar por dentro das novidades</h2>
+                    <div class="social-media">
+                        <ul class="list-social-media">
+                            <a class="link-social-media" href="#">
+                                <li class="item-social-media">
+                                    <i class="fab fa-facebook-f"></i>
+                                </li>
+                            </a>
+                            <a class="link-social-media" href="#">
+                                <li class="item-social-media">
+                                    <i class="fab fa-google"></i>
+                                </li>
+                            </a>
+                            <a class="link-social-media" href="#">
+                                <li class="item-social-media">
+                                    <i class="fab fa-linkedin-in"></i>
+                                </li>
+                            </a>
+                        </ul>
+                    </div><!-- social media -->
+                    <p class="description description-second">Ou use seu e-mail para entrar:</p>
+                    <form class="form">
+
+                        <label class="label-input" for="">
+                            <i class="far fa-envelope icon-modify"></i>
+                            <input type="email" placeholder="Email">
+                        </label>
+
+                        <label class="label-input" for="">
+                            <i class="fas fa-lock icon-modify"></i>
+                            <input type="password" placeholder="Senha">
+                        </label>
+
+                        <a href="#">Esqueceu a senha?</a>
+
+
+                        <button class="btn btn-second">sign in</button>
+                    </form>
+                </div>
+            </div>
         </div>
-    </div>
 </section>
